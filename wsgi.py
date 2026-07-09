b05f7e21fb6265cb881e4c5d5fe28c6625cd375d
stanionic <stanionic@users.noreply.github.com>
2026-07-09 06:53:31 -0930

    Fix wsgi.py quotes, update render.yaml with env vars

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 
+import os
 from app import create_app
 
-app = create_app()
+config_name = os.getenv("FLASK_ENV", "development")
+app = create_app(config_name)
 
 if __name__ == "__main__":
     app.run()
